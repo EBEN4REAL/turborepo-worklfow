8adeece0467923340bdd0809466d014e4d4160f6
Ebenezer Igbinoba <ebenezer.igbinoba@stu.cu.edu.ng>
2025-08-30 00:19:31 +0100

    update ci/cd workflow

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Required for S3/CloudFront static hosting
   output: 'export',
-  // If you use next/image, disable optimized loader for static export
-  images: { unoptimized: true },
-  // Optional but helps with S3 directory indexing
-  // trailingSlash: true,
+  images: { unoptimized: true }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
